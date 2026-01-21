64cb89d1fef526d744906bbc116169a31b09a54d
Aledve <jakile971012@gmail.com>
2026-01-21 18:12:03 -0500

    🔧update(ErrorMessage.tsx): update message

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import { ReactNode } from "react";
 
-function ErrorMessage() {
+export default function ErrorMessage({ children }: { children: ReactNode }) {
   return (
-    <div>
-      Error
+    <div className="text-center my-2 bg-red-100 text-red-600 font-bold p-2 uppercase text-sm rounded-lg border border-red-200">
+      {children}
     </div>
   )
 }
-
-export default ErrorMessage
